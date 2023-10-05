93f40f1ba78589c6464796aa1d92b1a4bed57c5a
gonzalez.espejo.alejandro <gonzalez.espejo.alejandro@alumnat.copernic.cat>
2023-10-05 13:44:12 +0200

    initial commit

---
+++ app/src/main/java/com/alejandroglzdev/yourhoroscope/ui/home/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.alejandroglzdev.yourhoroscope.ui.home
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.alejandroglzdev.yourhoroscope.R
+import com.alejandroglzdev.yourhoroscope.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initUI()
+    }
+
+    private fun initUI() {
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        val navHost =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHost.navController
+        binding.bottomBar.setupWithNavController(navController)
+    }
+}
