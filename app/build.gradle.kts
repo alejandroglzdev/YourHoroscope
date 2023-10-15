bea33ef61636e26c668d0d1f5e3e18df88c2d6bc
gonzalez.espejo.alejandro <gonzalez.espejo.alejandro@alumnat.copernic.cat>
2023-10-15 11:21:54 +0200

    Permissions done.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,8 @@ dependencies {
 
     val nav_version= "2.7.1"
     val hilt_version= "2.48"
+    val retrofit_version= "2.9.0"
+    val cameraVersion = "1.2.3"
 
     //Navigation Component
     implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
@@ -69,9 +71,16 @@ dependencies {
     kapt("com.google.dagger:hilt-android-compiler:$hilt_version")
 
     //Retrofit
-    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.squareup.retrofit2:retrofit:$retrofit_version")
+    implementation ("com.squareup.retrofit2:converter-gson:$retrofit_version")
     implementation ("com.squareup.okhttp3:logging-interceptor:4.3.1")
+
+    //Camera X
+    implementation ("androidx.camera:camera-core:${cameraVersion}")
+    implementation ("androidx.camera:camera-camera2:${cameraVersion}")
+    implementation ("androidx.camera:camera-lifecycle:${cameraVersion}")
+    implementation ("androidx.camera:camera-view:${cameraVersion}")
+    implementation ("androidx.camera:camera-extensions:${cameraVersion}")
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
--- app/src/main/java/com/alejandroglzdev/yourhoroscope/ui/palmistry/PalmistryFragment.kt
+++ app/src/main/java/com/alejandroglzdev/yourhoroscope/ui/palmistry/PalmistryFragment.kt
@@ -4,15 +4,54 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.PermissionChecker
 import androidx.fragment.app.Fragment
 import com.alejandroglzdev.yourhoroscope.databinding.FragmentPalmistryBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class PalmistryFragment : Fragment() {
+    companion object {
+        private const val PERMISSION_CAMERA = android.Manifest.permission.CAMERA
+    }
 
     private var _binding: FragmentPalmistryBinding? = null
     private val binding get() = _binding!!
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        if (isGranted) {
+
+        } else {
+            Toast.makeText(
+                requireContext(),
+                "Acepta los permisos para poder utilizar el apartado de Palmistry...",
+                Toast.LENGTH_LONG
+            ).show()
+
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        if (checkCameraPermission()) {
+
+        } else {
+            requestPermissionLauncher.launch(PERMISSION_CAMERA)
+
+        }
+    }
+
+    fun checkCameraPermission(): Boolean {
+        return PermissionChecker.checkSelfPermission(
+            requireContext(),
+            PERMISSION_CAMERA
+        ) == PermissionChecker.PERMISSION_GRANTED
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
